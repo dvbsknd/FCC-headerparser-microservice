3a60cf484db35923beee2e73b967261f763f86e1
Dave Baskind <dave@picketstudio.com>
2020-08-14 10:57:44 +1000

    Build the main route

--- routers/api.js
+++ routers/api.js
@@ -10,4 +10,11 @@ router.get("/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+router.get('/whoami', (req, res) => {
+  let ipaddress = req.ip;
+  let language = req.header('Accept-Language');
+  let software = req.header('User-Agent');
+  res.json({ ipaddress, language, software });
+});
+
 module.exports = router;
